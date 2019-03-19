fb6578ae27e608e6ac2bb891ddc48b77e8a33339
neven10 <nevenlalic10@gmail.com>
2019-03-19 15:13:54 +0100

    Utility

---
+++ MatchApp/UtilityApp/Entities/Match.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UtilityApp.Entities
+{
+        public class Rootobject
+        {
+            public Match[] Property1 { get; set; }
+        }
+
+        public class Match
+    {
+            public string EventID { get; set; }
+            public string Sport { get; set; }
+            public string HomeTeam { get; set; }
+            public string AwayTeam { get; set; }
+            public string Score { get; set; }
+            public DateTime StartTime { get; set; }
+            public int CurrentMinutes { get; set; }
+            public DateTime FinishTime { get; set; }
+            public Stake[] Stakes { get; set; }
+            public bool IsPause { get; set; }
+        }
+
+        public class Stake
+        {
+            public string StakeID { get; set; }
+            public string StakeKey { get; set; }
+            public float StakeValue { get; set; }
+        }
+
+    
+}
---
+++ MatchApp/UtilityApp/Services/MatchService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityApp.Entities;
+
+namespace UtilityApp.Services
+{
+    public class MatchService
+    {
+        private static HttpClient httpClient;
+
+
+        public async Task<Match> GetMatchTimeSpan(TimeSpan interval)
+        {
+            while (true)
+            {
+                
+                return await GetMatch();
+            }
+        }
+            
+             
+
+
+        private async Task<Match> GetMatch()
+        {
+            httpClient = new HttpClient();
+            using (var request = new HttpRequestMessage(HttpMethod.Get, "https://demo.aleacontrol.net/stakesimulator/api/matches"))
+            using (var response = await httpClient.SendAsync(request))
+            {
+                string content = await response.Content.ReadAsStringAsync();         //readasync i dump to string, a ako je json ogroman, onda streamreader
+                if(response.IsSuccessStatusCode==false)
+                {
+                    throw new Exception(((int)response.StatusCode).ToString() + content);  // "log"
+
+                }
+                return JsonConvert.DeserializeObject<Match>(content);
+            }
+
+
+        }
+    }
+}
